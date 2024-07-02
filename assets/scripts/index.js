f5178150082f4fe3be9ed86aded0571237f99916
Adam Hussain <adam@hussmedia.io>
2024-07-01 18:49:10 -0700

    copied over from previous file, started new repository due to git issues

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+const viewParks = document.getElementById('view-parks-planner')
+const viewReading = document.getElementById('view-reading-roundtable')
+
+viewParks.addEventListener('click', () => {
+
+})
+
+viewReading.addEventListener('click', () => {
+    
+})
