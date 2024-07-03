29a4c4ea132593a098d635394ff4a4b4bcf7bf94
Adam Hussain <adam@hussmedia.io>
2024-07-02 21:42:17 -0700

    put in correct application links and github repo links, anchor links to projects

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,36 @@
-const viewParks = document.getElementById('view-parks-planner')
-const viewReading = document.getElementById('view-reading-roundtable')
+// const viewParks = document.getElementById('view-parks-planner')
+// const viewReading = document.getElementById('view-reading-roundtable')
 
-viewParks.addEventListener('click', () => {
+// viewParks.addEventListener('click', () => {
 
-})
+// })
 
-viewReading.addEventListener('click', () => {
+// viewReading.addEventListener('click', () => {
     
-})
+// })
+
+document.getElementById('use-parks').addEventListener('click', function() {
+    window.open('https://adamh1223.github.io/parks-planner/', '_blank');
+});
+
+document.getElementById('parks-github').addEventListener('click', function() {
+    window.open('https://github.com/adamh1223/parks-planner', '_blank');
+});
+document.getElementById('use-reading').addEventListener('click', function() {
+    window.open('https://reading-roundtable-a1d1097f86e5.herokuapp.com/login', '_blank');
+});
+document.getElementById('reading-github').addEventListener('click', function() {
+    window.open('https://github.com/AdjoaHackman/reading-roundtable', '_blank');
+});
+document.getElementById('use-weather').addEventListener('click', function() {
+    window.open('https://adamh1223.github.io/weather-buddy/', '_blank');
+});
+document.getElementById('weather-github').addEventListener('click', function() {
+    window.open('https://github.com/adamh1223/weather-buddy', '_blank');
+});
+document.getElementById('use-coding-quiz').addEventListener('click', function() {
+    window.open('https://adamh1223.github.io/coding-quiz/', '_blank');
+});
+document.getElementById('coding-quiz-github').addEventListener('click', function() {
+    window.open('https://github.com/adamh1223/coding-quiz', '_blank');
+});
