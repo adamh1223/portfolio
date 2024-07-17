1932f285d2bbced7ae390b96aedf1330c513aa3f
Adam Hussain <adam@hussmedia.io>
2024-07-16 21:08:58 -0700

    added ecoffee

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,11 +13,11 @@ document.getElementById('use-reading').addEventListener('click', function() {
 document.getElementById('reading-github').addEventListener('click', function() {
     window.open('https://github.com/AdjoaHackman/reading-roundtable', '_blank');
 });
-document.getElementById('use-weather').addEventListener('click', function() {
-    window.open('https://adamh1223.github.io/weather-buddy/', '_blank');
+document.getElementById('use-coffee').addEventListener('click', function() {
+    window.open('https://e-coffee-7d6cb3a5dc4c.herokuapp.com/', '_blank');
 });
-document.getElementById('weather-github').addEventListener('click', function() {
-    window.open('https://github.com/adamh1223/weather-buddy', '_blank');
+document.getElementById('coffee-github').addEventListener('click', function() {
+    window.open('https://github.com/lelisiario/E-Coffee', '_blank');
 });
 document.getElementById('use-coding-quiz').addEventListener('click', function() {
     window.open('https://adamh1223.github.io/coding-quiz/', '_blank');
@@ -25,3 +25,22 @@ document.getElementById('use-coding-quiz').addEventListener('click', function() 
 document.getElementById('coding-quiz-github').addEventListener('click', function() {
     window.open('https://github.com/adamh1223/coding-quiz', '_blank');
 });
+document.getElementById('view-parks-planner').addEventListener('click', function() {
+    window.location.href ='https://adamh1223.github.io/portfolio/projects.html#parks';
+});
+document.getElementById('view-reading-roundtable').addEventListener('click', function() {
+    window.open('https://adamh1223.github.io/portfolio/projects.html#reading', '_blank');
+});
+
+document.getElementById('view-coding-quiz').addEventListener('click', function() {
+    window.location.href ='https://adamh1223.github.io/portfolio/projects.html#coding';
+});
+
+document.getElementById('view-coffee').addEventListener('click', function() {
+    window.open('https://adamh1223.github.io/portfolio/projects.html#coffee', '_blank');
+});
+
+
+// document.getElementById('view-coffee').addEventListener('click', function() {
+//     window.location.href ='https://adamh1223.github.io/portfolio/projects.html#coffee';
+// });
